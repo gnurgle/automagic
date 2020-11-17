b290d13c9f67d8544c0ca193ca22cc186f862a9e
Daniel Jamsheedy <djamsheedy99@gmail.com>
2020-11-17 00:02:26 -0500

    Get parts for given part type. - renamed the functions to better describe what is being passed - get_car_part_types() returns a list of part categories (part types) - the user will select a part type in the app, calling upon get_parts_for_part_type() to return a list of part names

--- ra_scraper.py
+++ ra_scraper.py
@@ -2,7 +2,7 @@ import requests
 from fuzzywuzzy import fuzz, process
 from bs4 import BeautifulSoup
 
-homeUrl = 'https://www.rockauto.com'
+homeLink = 'https://www.rockauto.com'
 catalog = '/en/catalog/'
 
 
@@ -11,7 +11,7 @@ def get_car_link( make, year, model, engine):
     # this function uses the parameters to find the desired link used to further parse for parts
 
     # construct our link given parameters
-    carUrl = '{0}{1},{2},{3}'.format(homeUrl + catalog, make, year, model)
+    carUrl = '{0}{1},{2},{3}'.format(homeLink + catalog, make, year, model)
     carPage = requests.get(carUrl)
 
     soup = BeautifulSoup(carPage.content, 'html.parser')
@@ -26,20 +26,36 @@ def get_car_link( make, year, model, engine):
     return soup.find('a', class_='navlabellink nvoffset nnormal', text=engineMatch)['href']
 
 
-def get_car_parts(make, year, model, engine):
+def get_car_part_types(make, year, model, engine):
 
     engineLink = get_car_link(make, year, model, engine)
     # gather the names of parts listed on rockauto
-    partsPage = requests.get(homeUrl + engineLink)
+    partsPage = requests.get(homeLink + engineLink)
     soup = BeautifulSoup(partsPage.content, 'html.parser')
 
     # gather name of listed car parts
     return [label.text for label in soup.find_all('a', class_='navlabellink nvoffset nnormal')][4:]
 
 
+def get_parts_for_part_type(make, year, model, engine, part):
+
+    engineLink = get_car_link(make, year, model, engine)
+    part = part.replace(' ', '+').lower()
+    partsLink = homeLink + engineLink + ',' + part
+
+    partPage = requests.get(partsLink)
+
+    soup = BeautifulSoup(partPage.content, 'html.parser')
+
+    tdLabels = soup.find_all('td', class_='nlabel')
+    partTypes = [label.a.text for label in tdLabels][5:]
+    return partTypes
+
+
 def scrape():
-    engineParts = get_car_parts('acura', 2008, 'tl', '3.2L V6 SOHC 24V')
-    print(engineParts)
+    carPartTypes = get_car_part_types('acura', 2008, 'tl', '3.2L V6 SOHC 24V')
+    parts = get_parts_for_part_type('acura', 2008, 'tl', '3.2L V6 SOHC 24V', carPartTypes[0])
+    print(parts)
 
 
 if __name__ == '__main__':
