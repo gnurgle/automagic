58a982f0b62e0d1d815d67e57e34bb8f566fc395
Joe Bellissimo <joeybellissimo@gmail.com>
2020-11-27 18:39:55 -0500

    added full screen picture

--- netCarShow.py
+++ netCarShow.py
@@ -4,5 +4,5 @@ import re
 
 def returnPictureURL(year, make, model):
     baseUrl = "https://www.netcarshow.com/"
-    addedUrl = baseUrl + make + '/' + year + '-' + model + '/'
+    addedUrl = baseUrl + make + '/' + year + '-' + model + '/' + "1280x960/wallpaper_01.htm"
     return addedUrl
