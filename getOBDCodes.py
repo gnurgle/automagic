7964c4ce8f8c0d172c059eac5cb72e074632eba4
Joe Bellissimo <joeybellissimo@gmail.com>
2020-11-27 16:56:46 -0500

    Put URLs in a url List

--- getOBDCodes.py
+++ getOBDCodes.py
@@ -3,103 +3,102 @@ from bs4 import BeautifulSoup
 import re
 import sqlite3 as sql
 
+
 def getCodes():
+    # Update this to work for all of the following pages
+    # https://www.obd-codes.com/trouble_codes/
+    # https://www.obd-codes.com/body-codes
+    # https://www.obd-codes.com/trouble_codes/obd-ii-c-chassis-codes.php
+    # https://www.obd-codes.com/trouble_codes/obd-ii-u-network-codes.php
+    # -----HERE------
 
-	#Update this to work for all of the following pages
-	#https://www.obd-codes.com/trouble_codes/
-	#https://www.obd-codes.com/body-codes
-	#https://www.obd-codes.com/trouble_codes/obd-ii-c-chassis-codes.php
-	#https://www.obd-codes.com/trouble_codes/obd-ii-u-network-codes.php
-	#-----HERE------
+    url2 = "https://www.obd-codes.com/body-codes"
+    url3 = "https://www.obd-codes.com/trouble_codes/obd-ii-c-chassis-codes.php"
+    url4 = "https://www.obd-codes.com/trouble_codes/obd-ii-u-network-codes.php"
 
-	url2 = "https://www.obd-codes.com/body-codes"
-	url3 = "https://www.obd-codes.com/trouble_codes/obd-ii-c-chassis-codes.php"
-	url4 = "https://www.obd-codes.com/trouble_codes/obd-ii-u-network-codes.php"
-	
-	#Url for scraping
-	url = "https://www.obd-codes.com/p00-codes"
+    # Url for scraping
+    url = list["https://www.obd-codes.com/p00-codes", "https://www.obd-codes.com/trouble_codes/",
+               "https://www.obd-codes.com/trouble_codes/obd-ii-c-chassis-codes.php", "https://www.obd-codes.com/trouble_codes/obd-ii-c-chassis-codes.php",
+               "https://www.obd-codes.com/trouble_codes/obd-ii-u-network-codes.php"]
 
-	#Fetch URL
-	response = requests.get(url)
-	response2 = requests.get(url2)
-	#Parse HTML
-	soup = BeautifulSoup(response.content, 'html.parser')
-	soup2 = BeautifulSoup(response2.content, 'html.parser')
-	#Grab list of all entries on page
-	codes = soup.find_all('li')
-	codes2 = soup.find_all('li')
-	#Split into Numbers and Names
-	codeNames = []
-	codeNumbers = []
-	for code in codes:
-		code = str(code)[21:-9]
-		if code.startswith('('):
-			code = ''
-		else:
-			codeNum = code[:5]
-			code = code[6:]
-			codeNames.append(code)
-			codeNumbers.append(codeNum)
+    # Fetch URL
+    response = requests.get(url)
+    # Parse HTML
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # Grab list of all entries on page
+    codes = soup.find_all('li')
+    # Split into Numbers and Names
+    codeNames = []
+    codeNumbers = []
+    for code in codes:
+        code = str(code)[21:-9]
+        if code.startswith('('):
+            code = ''
+        else:
+            codeNum = code[:5]
+            code = code[6:]
+            codeNames.append(code)
+            codeNumbers.append(codeNum)
 
-	#Finalize list (Only for first page, will need to change for others)
-	#-----HERE-----
-	codeNames = codeNames[7:-1]
-	codeNumbers = codeNumbers[7:-1]
+    # Finalize list (Only for first page, will need to change for others)
+    # -----HERE-----
+    codeNames = codeNames[7:-1]
+    codeNumbers = codeNumbers[7:-1]
 
-	#Insert a check here for P listings only
-	#----HERE-----
-	if codeNumbers[0] == 'P':
-		print("P listing valid for scraping")
-		return True
-	else:
-		print("Not a P listing, try again")
-		return False
-	#Send CodeNumber to page scraper
-	for num in codeNumbers:
-		codeDesc = getCodeInfo(num)
+    # Insert a check here for P listings only
+    # ----HERE-----DONE
+    if codeNumbers[0] == 'P':
+        print("P listing valid for scraping")
+        return True
+    else:
+        print("Not a P listing, try again")
+        return False
+    # Send CodeNumber to page scraper
+    for num in codeNumbers:
+        codeDesc = getCodeInfo(num)
 
-	#Commit CodeNames, CodeNumbers, and CodeDesc to DB
-	# --HERE--
-	#with sql.connect("car_base.zip") as con:
+
+# Commit CodeNames, CodeNumbers, and CodeDesc to DB
+# --HERE--
+# with sql.connect("car_base.zip") as con:
 
 def getCodeInfo(num):
+    # base url
+    url = "https://www.obd-data.com/" + num + ".html"
+    print(url)
+    # Fetch URL
+    response = requests.get(url)
 
-	#base url
-	url = "https://www.obd-data.com/" + num + ".html"
-	print (url)
-	#Fetch URL
-	response = requests.get(url)
+    # Parse HTML
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-	#Parse HTML
-	soup = BeautifulSoup(response.content, 'html.parser')
+    # Grab list of all entries on page
+    entry = soup.find('div', class_='entry-content')
 
-	#Grab list of all entries on page
-	entry = soup.find('div', class_='entry-content')
+    # Narrow down selection
+    results = soup.find_all('span')
 
-	#Narrow down selection
-	results = soup.find_all('span')
+    # Check for content
+    # ----HERE----DONE
+    test = soup.find_all(text=re.compile('Code Description'))
+    if test:
+        print("VALID PROBLEM CODE DETECTED")
+        return True
+    else:
+        print("INVALID PROBLEM CODE, TRY AGAIN")
+        return False
 
-	#Check for content
-	#----HERE----DONE
-	test = soup.find_all(text=re.compile('Code Description'))
-	if test:
-		print("VALID PROBLEM CODE DETECTED")
-		return True
-	else:
-		print("INVALID PROBLEM CODE, TRY AGAIN")
-		return False
+    # Filter html tags out
+    codeDesc = []
+    for result in results:
+        result = str(result)[6:-7]
+        codeDesc.append(result)
 
-	#Filter html tags out
-	codeDesc = []
-	for result in results:
-		result = str(result)[6:-7]
-		codeDesc.append(result)
-
-	#trim extra info
-	codeDesc = codeDesc[1:10]
-	print(codeDesc)
-	return (codeDesc)
+    # trim extra info
+    codeDesc = codeDesc[1:10]
+    print(codeDesc)
+    return (codeDesc)
 
 
 if __name__ == '__main__':
-	getCodes()
+    getCodes()
